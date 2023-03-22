7355186e5b3e77db64bba4200acee99f26ccd202
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-22 23:20:11 +0530

    5:42time

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Box, Container, HStack, Radio, RadioGroup } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { server } from "../index";
+import ErrorComponent from "./ErrorComponent";
 
 function CoinDetails() {
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/${params.id}`);
+        console.log(data);
+        setCoin(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoin();
+  }, [params.id]);
+
+  if (error) return <ErrorComponent message={"Error while fetching coin"} />;
+
   return (
-    <div>5:34:40</div>
-  )
+    <Container>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderWidth={1}>
+            5:42
+          </Box>
+
+          {/* button */}
+
+          <RadioGroup value={currency} onChange={setCurrency}>
+          <HStack>
+            <Radio value={"inr"}>₹ INR</Radio>
+            <Radio value={"usd"}>$ USD</Radio>
+            <Radio value={"eur"}>€ EURO</Radio>
+          </HStack>
+        </RadioGroup>
+        </>
+      )}
+    </Container>
+  );
 }
 
-export default CoinDetails
+export default CoinDetails;
